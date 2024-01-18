4da9dcba9b152ae82fa5e7207ff30126947710df
Rita <fedorova.margarita.a@gmail.com>
2024-01-19 00:24:53 +0300

    add HW_5

--- json_parsing.py
+++ json_parsing.py
@@ -3,6 +3,7 @@ sting_as_json_format = '{"answer":"Hello, User"}'
 obj = json.loads(sting_as_json_format)
 print(obj['answer'])
 print(json.loads('{"answer":"Hello, User"}')['answer'])
+print(json.loads(sting_as_json_format)['answer'])
 
 key = "answer"
 if key in obj:
