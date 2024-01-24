eb6c9d029cdfce79e265524f9977c60488c4f5c9
Rita <fedorova.margarita.a@gmail.com>
2024-01-25 00:35:25 +0300

    add lesson3_part_1

---
+++ Lesson_3/decorator.py
@@ -0,0 +1,14 @@
+def my_decorator(func):
+    def wrapper():
+        print("Что-то происходит перед вызовом функции")
+        func()
+        print("Что-то происходит после вызова функции")
+    return wrapper
+
+
+@my_decorator
+def say_hello():
+    print("Привет, мир!")
+
+
+say_hello()
---
+++ Lesson_3/test_first_api.py
@@ -0,0 +1,16 @@
+import requests
+class TestFirstApi:
+    def test_hello_call(self):
+        url = "https://playground.learnqa.ru/api/hello"
+        name = "Vitalii"
+        data = {"name": name}
+        response = requests.get(url, params=data)
+        assert response.status_code == 200, "Wrong response code"
+        response_dict = response.json()
+        assert "answer" in response_dict, "There is no fields answer in the response"
+        expected_response_text = f"Hello, {name}"
+        actual_response_text = response_dict["answer"]
+        assert expected_response_text == actual_response_text, "Actual text in the response is not correct"
+
+
+
