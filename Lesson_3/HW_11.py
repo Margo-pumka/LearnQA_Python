0c977a5ce9b0a4301fea287926229ee241679761
Rita <fedorova.margarita.a@gmail.com>
2024-01-28 11:24:05 +0300

    add lesson3_part_4

--- Lesson_3/HW_11.py
+++ Lesson_3/HW_11.py
@@ -1,14 +1,11 @@
-import json
 import requests
 class TestCookies:
     def test_cookies(self):
         response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
-        # print(dict(response.cookies))
-        # assert response.cookies, "There is no cookies in the response"
-        # assert "HomeWork" in response.cookies, "There is no field HomeWork in the cookies"
-        try:
-            assert response.cookies["HomeWork"] == "hw_value", f"Value of the field \'HomeWork\' is not \'hw_value\'"
-        except json.JSONEncoder:
-            "Cookies is not in JSON format"
+        print(dict(response.cookies))
+        assert response.cookies, "There is no cookies in the response"
+        assert "HomeWork" in response.cookies, "There is no field HomeWork in the cookies"
+        assert response.cookies["HomeWork"] == "hw_value", f"Value of the field \'HomeWork\' is not \'hw_value\'"
+
 
 
