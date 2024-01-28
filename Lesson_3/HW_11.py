8c808fea4748a4e504d3e5e1efb0f6c70a21dd6b
Rita <fedorova.margarita.a@gmail.com>
2024-01-28 17:49:47 +0300

    add lesson3_part_4

--- Lesson_3/HW_11.py
+++ Lesson_3/HW_11.py
@@ -4,7 +4,7 @@ class TestCookies:
         response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
         print(dict(response.cookies))
         assert response.cookies, "There is no cookies in the response"
-        assert "HomeWork" in response.cookies, "There is no field HomeWork in the cookies"
+        assert "HomeWork" in response.cookies, "There is no field \'HomeWork\' in the cookies"
         assert response.cookies["HomeWork"] == "hw_value", f"Value of the field \'HomeWork\' is not \'hw_value\'"
 
 
---
+++ Lesson_3/HW_12.py
@@ -0,0 +1,34 @@
+import requests
+import datetime
+class TestHeader:
+    def test_header(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_header")
+        print(response.headers)
+        expected_headers = {
+            'Date': '',
+            'Content-Type': 'application/json',
+            'Content-Length': '15',
+            'Connection': 'keep-alive',
+            'Keep-Alive': 'timeout=10',
+            'Server': 'Apache',
+            'x-secret-homework-header': 'Some secret value',
+            'Cache-Control': 'max-age=0',
+            'Expires': ''
+        }
+        for key, value in expected_headers.items():
+            assert key in response.headers, f"There is no header {key} in response"
+            if key == 'Date' or key == 'Expires':
+                current_time1 = (datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
+                    seconds=-2)).strftime('%a, %d %b %Y %H:%M:%S GMT')
+                current_time2 = (datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
+                    seconds=-1)).strftime('%a, %d %b %Y %H:%M:%S GMT')
+                i = [current_time1, current_time2]
+                assert (response.headers[key] == current_time1) or (response.headers[key] == current_time2), "Wrong current time"
+            else:
+                assert response.headers[key] == value, f"Header \'{key}\' has unexpected value \'{value}\'"
+
+
+
+
+
+
