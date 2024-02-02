7462de14338ad28f0688d1138e6806773a738578
Rita <fedorova.margarita.a@gmail.com>
2024-02-02 23:39:35 +0300

    add lesson3_part_4

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -35,6 +35,7 @@ class TestUserRegister(BaseCase):
         print(response.status_code)
         print(response.content)
         assert response.status_code == 400, f"Unexpected status code {response.status_code}"
-        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", \
+            f"Unexpected response content {response.content}"
 
 
