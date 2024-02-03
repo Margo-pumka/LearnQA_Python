e0e9811050d88c673b0b2ad736e99467704d76e2
Rita <fedorova.margarita.a@gmail.com>
2024-02-03 23:57:46 +0300

    add lesson4

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,7 +1,7 @@
 import pytest
-import requests
 from lib.assertions import Assertions
 from lib.base_case import BaseCase
+from lib.my_requests import MyRequests
 class TestUserAuth(BaseCase):
     exclude_params = [
         ("no_cookie"),
@@ -13,14 +13,14 @@ class TestUserAuth(BaseCase):
             "email": "vinkotov@example.com",
             "password": "1234"
         }
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        response1 = MyRequests.post("/user/login", data=data)
         self.auth_sid = self.getCookie(response1, "auth_sid")
         self.token = self.getHeader(response1,"x-csrf-token")
         self.user_id_from_auth_method = self.getJsonValue(response1, "user_id")
 
 
     def test_auth_user(self):
-        response2 = requests.get("https://playground.learnqa.ru/api/user/auth", headers={"x-csrf-token": self.token},
+        response2 = MyRequests.get("/user/auth", headers={"x-csrf-token": self.token},
                                  cookies={"auth_sid": self.auth_sid})
         Assertions.assert_json_value_by_name(response2, "user_id", self.user_id_from_auth_method,
                                              "User id from auth method is not equal user id from check method")
@@ -28,9 +28,9 @@ class TestUserAuth(BaseCase):
     @pytest.mark.parametrize('condition', exclude_params)
     def test_negative_auth_check(self, condition):
         if condition == "no_cookie":
-            response2 = requests.get("https://playground.learnqa.ru/api/user/auth", headers={"x-csrf-token": self.token})
+            response2 = MyRequests.get("/user/auth", headers={"x-csrf-token": self.token})
         else:
-            response2 = requests.get("https://playground.learnqa.ru/api/user/auth", cookies={"auth_sid": self.auth_sid})
+            response2 = MyRequests.get("/user/auth", cookies={"auth_sid": self.auth_sid})
 
         Assertions.assert_json_value_by_name(response2, "user_id", 0,
                                              f"User authorized with condition {condition}")
--- tests/test_user_register1.py
+++ tests/test_user_register1.py
@@ -1,11 +1,11 @@
-import requests
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 class TestUserRegister1(BaseCase):
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
@@ -13,7 +13,7 @@ class TestUserRegister1(BaseCase):
         email = "vinkotov@example.com"
         data = self.prepare_registration_data(email)
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
         print(response.status_code)
         print(response.content)
         assert response.status_code == 400, f"Unexpected status code {response.status_code}"
