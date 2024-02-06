8bcf63cdef66867c637f4a9465d51237849a2a3e
Rita <fedorova.margarita.a@gmail.com>
2024-02-06 22:41:52 +0300

    add lesson4

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -29,3 +29,31 @@ class TestUserGet(BaseCase):
         # Assertions.assert_json_has_key(response2, "first_name")
         # Assertions.assert_json_has_key(response2, "last_name")
 
+    def test_get_user_details_auth_as_other_user(self):
+
+        data1 = self.prepare_registration_data()
+        response1reg = MyRequests.post("/user/", data=data1)
+        del data1["username"]
+        del data1["firstName"]
+        del data1["lastName"]
+        response1auth = MyRequests.post("/user/login", data=data1)
+        auth_sid1 = self.getCookie(response1auth, "auth_sid")
+        token1 = self.getHeader(response1auth, "x-csrf-token")
+
+
+        data2 = self.prepare_registration_data()
+        response2reg = MyRequests.post("/user/", data=data2)
+        del data2["username"]
+        del data2["firstName"]
+        del data2["lastName"]
+        response2auth = MyRequests.post("/user/login", data=data2)
+        user_id_from_auth_method2 = self.getJsonValue(response2auth, "user_id")
+
+        response3 = MyRequests.get(f"/user/{user_id_from_auth_method2}",
+                                 headers={"x-csrf-token": token1}, cookies={"auth_sid": auth_sid1})
+        unexpected_fields = ["email", "firstName", "lastName"]
+        Assertions.assert_json_has_key(response3, "username")
+        Assertions.assert_json_has_not_keys(response3, unexpected_fields)
+
+
+
